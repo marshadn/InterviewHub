5427d1edd4daa6e51ebae9001585c5d5bad71116
marshadn <muhamedmarshad18@gmail.com>
2024-10-08 20:32:05 +0530

    fixes in index.jsx

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+// import React from 'react'
+import AddNewInterview from '../components/custom/AddNewInterview'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className='p-10 '>
+      <h2 className='font-bold text-2xl'>Dashboard</h2>
+      <h2 className='text-gray-500'>Create and Start Your Interview</h2>
+      <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
+        <AddNewInterview/>
+      </div>
+    </div>
   )
 }
 
