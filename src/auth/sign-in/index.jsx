a1f2dc2ee5018c8be52ac52ae3ab791fbbb5d2e6
marshadn <muhamedmarshad18@gmail.com>
2024-10-27 11:20:57 +0530

    style auth-page

--- src/auth/sign-in/index.jsx
+++ src/auth/sign-in/index.jsx
@@ -1,12 +1,25 @@
-// import React from 'react'
+// // import React from 'react'
+// import { SignIn } from "@clerk/clerk-react";
+
+// function SignInPage() {
+//   return (
+//     <div className="flex justify-center py-7">
+//       <SignIn />
+//     </div>
+//   );
+// }
+
+// export default SignInPage;
+
 import { SignIn } from "@clerk/clerk-react";
 
 function SignInPage() {
   return (
-    <div className="flex justify-center py-7">
-      <SignIn />
+    <div className="flex justify-center items-center min-h-screen py-7">
+      <SignIn afterSignInUrl="/dashboard" /> {/* Redirects to /dashboard after sign-in */}
     </div>
   );
 }
+
 
 export default SignInPage;
