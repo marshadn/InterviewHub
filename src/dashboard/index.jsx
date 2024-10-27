b41483ac1b1edbba025789d02533738afceae41d
marshadn <muhamedmarshad18@gmail.com>
2024-10-27 11:19:33 +0530

    Dashbord Styles

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -4,8 +4,8 @@ import AddNewInterview from '../components/custom/AddNewInterview'
 function Dashboard() {
   return (
     <div className='p-10 '>
-      <h2 className='font-bold text-2xl'>Dashboard</h2>
-      <h2 className='text-gray-500'>Create and Start Your Interview</h2>
+      <h2 className='font-bold text-2xl text-primary font-sfpro'>Dashboard</h2>
+      <h2 className='text-gray-500 font-sfpro'>Create and Start Your Interview</h2>
       <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
         <AddNewInterview/>
       </div>
